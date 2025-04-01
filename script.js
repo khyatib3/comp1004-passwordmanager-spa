3c61c059528c4378f58dc3ff0843c0b60a319810
khyat <khyatib@live.co.uk>
2025-04-01 21:44:22 +0100

    strength bar still not showing for strong password, 2 sections on home page still showing (these needs to be fixed). added learn more tab in nav bar.

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ let userIDHash = "";
 let masterKey = "";
 
 //function to show the relevant section when needed, so all sections don't get displayed automatically
-function showSection(sectionId){
+function showSection(sectionId) {
     //hiding all the sections
-    document.querySelectorAll('.section').forEach(section =>{
+    document.querySelectorAll('.section').forEach(section => {
         section.classList.remove('active');
     })
 
     //then showing the selected section
     let selectedSection = document.getElementById(sectionId);
-    if(selectedSection){
+    if (selectedSection) {
         selectedSection.classList.add('active');
-    }else {
+    } else {
         console.error("Couldn't find section:", sectionId);
     }
 }
 
 //function which is used for hashing usernames and passwords
 //will be used to make the masterKey
-async function hash(value){
+async function hash(value) {
     const encoder = new TextEncoder();
     const data = encoder.encode(value);
     const hashedValue = await crypto.subtle.digest('SHA-256', data);
@@ -40,9 +40,9 @@ async function hash(value){
 
 //method that checks whether the suer has logged into PH before they choose to see accounts.
 //ensures security
-function checkUserLoggedIn(sectionId){
+function checkUserLoggedIn(sectionId) {
     let userLoggedIn = localStorage.getItem("loggedIn");
-    if(userLoggedIn !== "true"){
+    if (userLoggedIn !== "true") {
         alert("You must login to PasswordHaven first!");
         //only show them home section so they either sign up or log in
         showSection("home");
@@ -51,15 +51,108 @@ function checkUserLoggedIn(sectionId){
     showSection(sectionId);
 }
 
-function readFromLocalStorage(readItem){
+function readFromLocalStorage(readItem) {
     return JSON.parse(localStorage.getItem(readItem.toString()));
     //decryptPassword()
 }
 
-function writeToLocalStorage(key, value){
+function writeToLocalStorage(key, value) {
     //encryptPassword()
     localStorage.setItem(value, JSON.stringify(value));
 }
+
+function displayPasswordStrength() {
+    let password = document.getElementById("enteredPassword").value;
+    let indicator = document.getElementById("passwordStrengthIndicator");
+    let strengthBar = document.getElementById("passwordStrengthBar");
+    let passwordStrength = calculatePasswordStrength(password);
+
+    let width = 0;
+    let colour = "red";
+    if (passwordStrength === "Weak") {
+        width = 30;
+        colour = "red";
+        indicator.innerText = "You should make it longer and add more numbers, special characters and capitals!";
+        indicator.classList.add("text-danger");
+    } else if (passwordStrength === "Medium") {
+        width = 60;
+        colour = "orange";
+        indicator.innerText = "Try adding more characters, and add a few numbers!";
+        indicator.classList.remove("text-danger");
+        indicator.classList.add("text-warning");
+    } else if (passwordStrength === "Strong") {
+        width = 100;
+        colour = "green";
+        indicator.innerText = "Perfect! This is a strong password to use.";
+        indicator.classList.remove("text-warning");
+        indicator.classList.add("text-success");
+    }
+
+    //applying the styles
+    strengthBar.style.width = width + "%";
+    strengthBar.style.backgroundColor = colour;
+
+}
+
+function calculatePasswordStrength(password) {
+    //creating a strength 'score'
+    let strengthScore = 0;
+
+    //checking length of password
+    if (password.length === 0) { // if they typed nothing
+        return "Weak";
+    }
+    if (password.length >= 2 && password.length <=7) {
+        strengthScore += 1;
+    }else if (password.length >= 8) {
+        strengthScore += 2;
+    }
+
+    //checking for uppercase characters
+    let hasUpperCase = /[A-Z]/.test(password) ? 1 : 0;
+
+    //checking for lowercase characters
+    let hasLowerCase = /[a-z]/.test(password) ? 1 : 0;
+
+    //checking for numbers
+    let hasNumbers = /[0-9]/.test(password) ? 1 : 0;
+
+    //checking for special chars
+    let hasSpecialCharacters= /[!"£$%^&*()_+-={}[]:@~#<>?]/.test(password) ? 1 : 0;
+
+    strengthScore = strengthScore + hasSpecialCharacters + hasNumbers + hasLowerCase + hasUpperCase;
+
+    if (strengthScore <= 3) {
+        return "Weak";
+    } else if (strengthScore >= 3 && strengthScore <= 5) {
+        return "Medium"
+    } else {
+        return "Strong";
+    }
+}
+
+//live message display for confirm password input against password input
+document.getElementById("confirmPassword").addEventListener("input", function () {
+    let password = document.getElementById("enteredPassword").value;
+    let confirmPassword = this.value;
+    let confirmMsg = document.getElementById("confirmPasswordMsg");
+
+    if (confirmPassword === "") {
+        confirmMsg.innerText = "No password detected!";
+        confirmMsg.classList.add("text-warning");
+    } else if (confirmPassword === password) {
+        confirmMsg.innerText = "Passwords match!";
+        confirmMsg.classList.remove("text-danger");
+        confirmMsg.classList.remove("text-warning");
+        confirmMsg.classList.add("text-success");
+    } else {
+        confirmMsg.innerText = "Passwords do not match! Please try again!";
+        confirmMsg.classList.remove("text-success");
+        confirmMsg.classList.remove("text-warning");
+        confirmMsg.classList.add("text-danger");
+    }
+
+});
 
 class User {
     constructor(username, password) {
@@ -74,7 +167,7 @@ class User {
         if (hashedFromLocalStorage !== null) {
             alert("An account already exists. Please login!")
             return false;
-        }else{
+        } else {
             let hashedPassword = hash(password);
 
             //initiating an instance of the user
@@ -82,7 +175,7 @@ class User {
 
             // adding the user to local storage
             writeToLocalStorage(JSON.stringify(newUser));
-            
+
             //creating the master key by hashing a combination of username and password
             masterKey = hash(username + password);
             return true;
@@ -103,7 +196,7 @@ class User {
         let hashedPasswordFromLocalStorage = readFromLocalStorage(userIDHash);
         if (hashedPasswordFromLocalStorage == null) {
             return false;
-        }else{
+        } else {
             let computedHashedPassword = hash(inputPassword);
             if (computedHashedPassword == hashedPasswordFromLocalStorage) {
                 masterKey = hash(username + inputPassword);
@@ -131,7 +224,7 @@ class User {
 
         if (success) {
             //changing the sign-up message to notify the user
-            signUpMsg.innerText = 'Your account was created successfully! Please login now!';
+            signUpMsg.innerText = '✅Your account was created successfully! Please login now!';
             signUpMsg.classList.remove('text-danger');
             signUpMsg.classList.add('text-success');
 
@@ -148,7 +241,7 @@ class User {
             // }, 1000); //close after 1 second
 
         } else {
-            signUpMsg.innerText = '⚠️ Your account with us already exists! Please login.';
+            signUpMsg.innerText = "⚠️ Your account with us already exists! Please login.";
             signUpMsg.classList.remove('text-success');
             signUpMsg.classList.add('text-danger');
         }
@@ -166,7 +259,7 @@ class User {
         if (detailsMatch) {
             localStorage.setItem("loggedIn", "true");
 
-            loginMsg.innerHTML = "Successful Login!";
+            loginMsg.innerHTML = "✅Successful Login!";
             loginMsg.classList.remove('text-danger');
             loginMsg.classList.add('text-success');
             // //closing the modal afterward
@@ -202,7 +295,7 @@ class PasswordManager {
     }
 
     //adding account to local storage method
-    static addAccount(){
+    static addAccount() {
         let site = document.getElementById('siteName').value;
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
@@ -210,125 +303,33 @@ class PasswordManager {
         let dateOfCreation = document.getElementById('enteredDate').value;
         let accountAddMsg = document.getElementById('accountAddMsg');
 
+        //check that both the passwords match before proceeding
+        if (password !== confirmPassword) {
+            accountAddMsg.innerText = "❌Passwords do not match! Please check again!";
+            accountAddMsg.classList.remove("text-success");
+            accountAddMsg.classList.add("text-danger");
+            return;
+        }
+
         //checking if that account already exists
         let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
         let accountExists = accounts.some(account => account.site === site && account.username === username && account.password === password);
         if (accountExists) {
-            accountAddMsg.innerText = "This account already exists in the password manager! You can find it in 'View Accounts'.";
+            accountAddMsg.innerText = "⚠️This account already exists in the password manager! You can find it in 'View Accounts'.";
             accountAddMsg.classList.remove('text-success');
             accountAddMsg.classList.add('text-danger');
-        } else{
-            if(password === confirmPassword){
-                    passwordStrengthMsg.innerText = "Good! This is a strong password to use.";
-                    passwordStrengthMsg.classList.remove("text-danger");
-                    passwordStrengthMsg.classList.add("text-success");
-
-                    PasswordManager.saveAccount(site, email, password);
-                    accountAddMsg.innerText = "Your account has been saved successfully!";
-                    accountAddMsg.classList.remove('text-danger');
-                    accountAddMsg.classList.add('text-success');
-                    showSection('accountSuccessfulAddMsg');
-                }else{
-                    passwordStrengthMsg.innerText = "Hmm. This password isn't quite secure, please try our password generator or review our guidelines on how to create a strong password.";
-                    passwordStrengthMsg.classList.remove('text-warning');
-                }
-
-
+        } else {
+            PasswordManager.saveAccount(site, email, password);
+            accountAddMsg.innerText = "✅ Your account has been saved successfully!";
+            accountAddMsg.classList.remove('text-danger');
+            accountAddMsg.classList.add('text-success');
+            showSection('accountSuccessfulAddMsg');
         }
     }
-
-}
-function displayPasswordStrength(password) {
-    password = document.getElementById("enteredPassword").value;
-    let indicator = document.getElementById("passwordStrengthIndicator");
-    let strengthBar = document.getElementById("passwordStrengthBar");
-    let passwordStrength = calculatePasswordStrength(password);
-
-    let width = 0;
-    let colour = "red";
-    if(passwordStrength === "Weak"){
-        width = 30;
-        colour = "red";
-        indicator.innerText = "You should make it longer and add more numbers, special characters and capitals!";
-        indicator.classList.add("text-danger");
-    } else if (passwordStrength === "Medium"){
-        width = 60;
-        colour = "orange";
-        indicator.innerText = "Try adding more characters, and add a few numbers!";
-        indicator.classList.remove("text-danger");
-        indicator.classList.add("text-warning");
-    } else if (passwordStrength === "Strong"){
-        width = 100;
-        colour = "green";
-        indicator.innerText = "Perfect! This is a strong password to use.";
-        indicator.classList.remove("text-warning");
-        indicator.classList.add("text-success");
-    }
-
-    //applying the styles
-    strengthBar.style.width = width + "%";
-    strengthBar.style.backgroundColor = colour;
-
 }
 
-function calculatePasswordStrength(password){
-    //creating a strength 'score'
-    let strengthScore = 0;
-
-    //checking length of password
-    if(password.length ===0){ // if they typed nothing
-        strengthScore = 0;
-    }
-    if(password.length <=7){
-        strengthScore++;
-    }
-    if(password.length >= 12){
-        strengthScore += 2;
-    }
-
-    //checking for uppercase characters
-    let hasUpperCase;
-    if (/[A-Z]/.test(password)) {
-        hasUpperCase = 1;
-    } else {
-        hasUpperCase = 0;
-    }
-
-    //checking for lowercase characters
-    let hasLowerCase;
-    if (/[a-z]/.test(password)) {
-        hasLowerCase = 1;
-    } else {
-        hasLowerCase = 0;
-    }
-
-    //checking for numbers
-    let hasNumbers;
-    if (/[0-9]/.test(password)) {
-        hasNumbers = 1;
-    } else {
-        hasNumbers = 0;
-    }
-
-    //checking for special chars
-    let hasSpecialCharacters;
-    if (/[!"£$%^&*()_+-={}[]:@~#<>?]/.test(password)) {
-        hasSpecialCharacters = 1;
-    } else {
-        hasSpecialCharacters = 0;
-    }
-
-    strengthScore = strengthScore + hasSpecialCharacters + hasNumbers + hasLowerCase + hasUpperCase;
-    if(strengthScore <= 1){
-        return "Weak";
-    } else if (strengthScore >= 1 && strengthScore <= 5){
-        return "Medium"
-    } else if (strengthScore > 5){
-        return "Strong";
-    }
-}
 // Account class below
-class Account{
+class Account {
     constructor(siteName, url, username, password, dateAdded) {
         this.siteName = siteName;
         this.url = url;
@@ -360,7 +361,7 @@ class Account{
         const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0').join(''));
 
         //store the salt and the hash
-        return {salt : saltHex, hash: hashHex};
+        return {salt: saltHex, hash: hashHex};
     }
 
     static async aesEncryptPassword(password, key) {
@@ -368,19 +369,19 @@ class Account{
         const encodedData = this.textToUint8Array(password);
 
         const encryptedData = await crypto.subtle.encrypt(
-            {name : "AES-GCM", iv},
+            {name: "AES-GCM", iv},
             key,
             encodedData
         );
 
-        return{
-            iv : Array.from(iv), // convert to array to store
+        return {
+            iv: Array.from(iv), // convert to array to store
             encrypted: Array.from(new Uint8Array(encryptedData))
         };
     }
 
     //this is to convert a string into a Uint8Array
-    static textToUint8Array (text){
+    static textToUint8Array(text) {
         return new TextEncoder().encode(text);
     }
 
@@ -389,7 +390,7 @@ class Account{
         return new TextEncoder().encode(uint8Array);
     }
 
-    async generateKey(){
+    async generateKey() {
         return await crypto.subtle.generateKey(
             algorithm,
             true,
@@ -397,13 +398,13 @@ class Account{
         );
     }
 
-    static async decryptPassword(encryptedPassword, key){
+    static async decryptPassword(encryptedPassword, key) {
         const {iv, encrypted} = encryptedPassword;
         const encryptedArray = new Uint8Array(encrypted);
         const ivArray = new Uint8Array(iv);
 
         const decryptedPassword = await crypto.subtle.decrypt(
-            {name : "AES-GCM", iv: iv, ivArray},
+            {name: "AES-GCM", iv: iv, ivArray},
             key,
             encryptedArray
         );
@@ -411,8 +412,6 @@ class Account{
     }
 
 }
-
-
 
 class PasswordGenerator {
     static generatePassword() {
@@ -424,7 +423,7 @@ class PasswordGenerator {
         this.showGeneratedPassword(password);
     }
 
-    static showGeneratedPassword(password){
+    static showGeneratedPassword(password) {
         let message = document.getElementById("generatedPassword");
         message.innerText = password;
         message.classList.remove("error");
